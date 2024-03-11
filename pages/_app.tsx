c3b76e25244a81e618385de2f5ceb674fa607c07
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-03-11 15:23:04 -0300

    Configuração dos projeto, configuração das pastas e dos arquivos e instalação das libs que serão usadas.

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-//depois quando formos iniciar o backend, nós vamos parar esse servidor, pois o backend vai rodar sempre na porta 3000, e se você iniciar apenas o front, ele vai pegar a porta 3000. Então vamos parar o front, rodar o back e depois rodar o front de novo, para ficar na parta 3001.
