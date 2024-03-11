fd8279a1e77717b2a438dca6813adea5fef44a85
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-03-11 18:43:06 -0300

    Configuração de pastas e componentes e variáveis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+//depois quando formos iniciar o backend, nós vamos parar esse servidor, pois o backend vai rodar sempre na porta 3000, e se você iniciar apenas o front, ele vai pegar a porta 3000. Então vamos parar o front, rodar o back e depois rodar o front de novo, para ficar na parta 3001.
