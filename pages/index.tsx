166190feccc935a26b524360fab317a8b1dfe81f
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-03-19 16:28:06 -0300

    Criação dos elementos, do visual e também da sua responsividade - Seção de apresentação, homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,26 @@
 import Head from "next/head";
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
-import styles from "../styles/homeNoAuth.module.scss" 
+import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
+import styles from "../styles/homeNoAuth.module.scss"
 
-const HomeNoAuth = () =>{
-   return(
+const HomeNoAuth = () => {
+  return (
     <>
-    <Head>
-    <title>Onebitflix</title>
-      <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-      <meta property="og:title" content="Onebitflix" key="title" />
-			<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."/>
-    </Head>
-    <main>
-	<HeaderNoAuth />
-</main>
+      <Head>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
+      </Head>
+      <main>
+        <div className={styles.sectionBackground}>
+        <HeaderNoAuth />
+        <PresentationSection />
+        </div>
+        
+      </main>
     </>
-   )
+  )
 }
 
 export default HomeNoAuth;
