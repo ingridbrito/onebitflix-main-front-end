ccd64da3502d26b83752ac58ed6ed12cc262605c
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-04-05 11:14:27 -0300

    Instalação do AOS e finalização do header genérico

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,23 @@ import styles from "../styles/homeNoAuth.module.scss"
 import SlideSection from "@/src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "@/src/services/courseService";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "@/src/components/common/footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 interface IndexPageProps {
   children?: ReactNode;
   course: CourseType[];
 }
 
-const HomeNoAuth = ({course}: IndexPageProps) => {
+const HomeNoAuth = ({ course }: IndexPageProps) => {
+
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
   return (
     <>
       <Head>
@@ -24,12 +32,22 @@ const HomeNoAuth = ({course}: IndexPageProps) => {
         <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
-        <HeaderNoAuth />
-        <PresentationSection />
+        <div className={styles.sectionBackground}
+          data-aos="fade-zoom-in"
+          data-aos-duration="1600">
+          <HeaderNoAuth />
+          <PresentationSection />
         </div>
-        <CardSection />
-        <SlideSection newestCourses={course}/>
+        <div
+          data-aos="fade-right"
+          data-aos-duration="1200" >
+          <CardSection />
+        </div>
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1450" >
+          <SlideSection newestCourses={course} />
+        </div>
         <Footer />
       </main>
     </>
---
+++ pages/register.tsx
@@ -0,0 +1,21 @@
+import styles from "../styles/registerLogin.module.scss";
+import Head from "next/head";
+import Footer from "@/src/components/common/footer";
+import HeaderGeneric from "@/src/components/common/headerGeneric";
+
+const Register = () => {
+    return (
+        <>
+            <Head>
+                <title>Onebitflix - Registro</title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+
+            </Head>
+            <main>
+                <HeaderGeneric logoUrl="/" btnUrl="/login" btnContent="Quero fazer login"/>
+            </main>
+        </>
+    );
+};
+
+export default Register;
