51b8f87d69538f5c05e9f8a49c250483fcdf0db1
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-04-09 18:22:05 -0300

    Criação da seção curso destaque

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,19 +1,19 @@
 import HeaderAuth from "@/src/components/common/headerAuth"
+import FeaturedSection from "@/src/components/homeAuth/featuredSection"
 import Head from "next/head"
 import { useEffect } from "react"
 
 1
-const HomeAuth = function (){
-    return(
+const HomeAuth = function () {
+    return (
         <>
-        <Head>
-            <title> Onebitflix - Home </title>
-            <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-        </Head>
-        <main>
-            <HeaderAuth/>
-
-        </main>
+            <Head>
+                <title> Onebitflix - Home </title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+            </Head>
+            <main>
+                <FeaturedSection />
+            </main>
         </>
     )
 }
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import api from "./api";
 
 export type EpisodeType = {
@@ -25,7 +26,25 @@ getNewestCourse: async () => {
     });
 
     return res;
+},
+getFeaturedCourses: async () => {
+	const token = sessionStorage.getItem("onebitflix-token");
+
+  const res = await api
+  .get("/courses/featured", {
+   headers: {
+	   Authorization: `Bearer ${token}`,
+   },
+   })
+   .catch((error) => {
+   console.log(error.response.data.message);
+
+   return error.response;
+   });
+
+   return res;
 }
 }
+
 
 export default courseService
