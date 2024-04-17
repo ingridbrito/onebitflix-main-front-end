74742d05b1a688196833036c8e2ff62e9681d373
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-04-17 13:09:43 -0300

    Conexão do backend relacionado aos dados do usuário. Criação de um serviço e também de um método e também de toda a parte de conexão com o front - Conexão backend dados do usuário

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -2,8 +2,9 @@ import { Container, Form, Input } from "reactstrap";
 import Modal from "react-modal";
 import styles from "./styles.module.scss";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import profileService from "@/src/services/profileService";
 
 Modal.setAppElement("#__next");
 
@@ -11,7 +12,16 @@ Modal.setAppElement("#__next");
 const HeaderAuth = function () {
     const router = useRouter()
     const [modalIsOpen, setmodalOpen] = useState(false);
+    const [initials, setInitials] = useState("");
 
+
+    useEffect(() => {
+        profileService.fetchCurrent().then((user) => {
+          const firstNameInitial = user.firstName.slice(0, 1);
+        const lastNameInitial = user.lastName.slice(0, 1);
+        setInitials(firstNameInitial + lastNameInitial);
+      });
+    }, []);
 
     const handleOpenModal = () => {
         setmodalOpen(true)
@@ -43,7 +53,7 @@ const HeaderAuth = function () {
                             className={styles.input} />
                     </Form>
                     <img src="/homeAuth/iconSearch.svg" alt="lupaHeader" className={styles.searchImg} />
-                    <p className={styles.userProfile} onClick={handleOpenModal}>AB</p>
+                    <p className={styles.userProfile} onClick={handleOpenModal}>	{initials}</p>
                 </div>
                 <Modal
                     isOpen={modalIsOpen}
