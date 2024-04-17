5c25520aa59460aea90ed949cd15b3297d7408ee
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-04-17 15:35:47 -0300

    Criação do Page Spinner

--- src/components/homeAuth/favoriteCategory/index.tsx
+++ src/components/homeAuth/favoriteCategory/index.tsx
@@ -2,13 +2,14 @@ import useSWR from "swr";
 import styles from "../../../../styles/slideCategory.module.scss"
 import courseService from "@/src/services/courseService";
 import SlideComponent from "../../common/slideComponent";
+import PageSpinner from "../../common/spinner";
 
 
 const FavoriteCourses = function () {
 
     const { data, error } = useSWR("/favCourses", courseService.getFavCourses);
     if (error) return error;
-    if (!data) return <> <p>Loading ...</p></>
+    if (!data) return <> <PageSpinner/> </>
 
     return <>
         <p className={styles.titleCategory}> Minha Lista</p>
--- src/components/homeAuth/featuredSection/index.tsx
+++ src/components/homeAuth/featuredSection/index.tsx
@@ -4,17 +4,13 @@ import courseService, { CourseType } from "@/src/services/courseService";
 import HeaderAuth from "../../common/headerAuth";
 import Link from "next/link";
 import { Button, Container } from "reactstrap";
+import PageSpinner from "../../common/spinner";
 
 const FeaturedSection = function () {
     const { data, error } = useSWR("/featured", courseService.getFeaturedCourses);
     if (error) return error;
-    if (!data)
-        return (
-            <>
-                        <p>Loading...</p>
+    if (!data) return <> <PageSpinner/> </>
 
-            </>
-        )
 
     return (
         <>
