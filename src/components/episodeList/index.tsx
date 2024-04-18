286c182774c1225a504678edfa2e7aefcde26ecc
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-04-18 15:04:07 -0300

    Criação dos elementos da página de espisódio, do seu estilo visual e também da sua responsividade completa - Episódio

--- src/components/episodeList/index.tsx
+++ src/components/episodeList/index.tsx
@@ -1,11 +1,19 @@
-import { EpisodeType } from "@/src/services/courseService";
+import { CourseType, EpisodeType } from "@/src/services/courseService";
 import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
 
 interface props {
     episode: EpisodeType;
+    course: CourseType;
 }
 
-const handleSecondsToMin = (totalSeconds: number) => {
+
+const EpisodeList = function ({episode, course}: props){
+
+const router = useRouter();
+
+
+  const handleSecondsToMin = (totalSeconds: number) => {
     const minutes = Math.floor(totalSeconds / 60);
 
     const seconds = totalSeconds % 60;
@@ -19,9 +27,12 @@ const handleSecondsToMin = (totalSeconds: number) => {
     return result;
   };
 
-const EpisodeList = function ({episode}: props){
+  const handleEpisodePlayer = () =>{
+    router.push(`/courses/episode/${episode.order - 1}?courseid=${course.id}`)
+  }
+
     return <>
-   <div className={styles.episodeCard}>
+   <div className={styles.episodeCard} onClick={handleEpisodePlayer}>
 	  <div className={styles.episodeOrderTime}>
 	    <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
       <p className={styles.episodeTime}>{handleSecondsToMin(episode.secondsLong)}</p>
